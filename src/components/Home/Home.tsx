69f65d0708b369a6aaa9108a8f243cf13bdc2606
Jakub.Dragowski <kuba.dragowski@gmail.com>
2025-05-09 20:21:04 +0200

    Create HomePage -Add styles to component - Add custom logo - Create button with hover styling

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,23 @@
+import { Link } from 'react-router-dom';
+import logoTitle from '../../assets/images/logo_jd_small.png';
 import classes from './Home.module.scss';
 
 const Home = () => {
   return (
     <div className={`${classes.container} ${classes.homePage}`}>
-      <div>
-        <h1>Hi Hello</h1>
+      <div className={classes['text-zone']}>
+        <h1>
+          Hi,
+          <br /> I'm
+          <img src={logoTitle} alt="developer" />
+          acob
+          <br />
+          Web Developer
+        </h1>
+        <h2>Frontend Developer/ HTML/SCSS/JAVASCRIPT/REACT</h2>
+        <Link to="/contact" className={classes['flat-button']}>
+          Contact Me
+        </Link>
       </div>
     </div>
   );
