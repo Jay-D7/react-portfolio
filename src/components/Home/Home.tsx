6cea64a4bbede7fb95bc9c7ae227b485d0e038c0
Jakub.Dragowski <kuba.dragowski@gmail.com>
2025-05-09 03:50:41 +0200

    Add Home component and integrate it into the main application layout with Route

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,13 @@
+import classes from './Home.module.scss';
+
+const Home = () => {
+  return (
+    <div className={`${classes.container} ${classes.homePage}`}>
+      <div>
+        <h1>Hi Hello</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
